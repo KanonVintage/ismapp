36fc39d7dd15046c817d333d84962a568cb3fb38
KanonVitage <guido.duchi@gmail.com>
2018-09-14 15:58:56 -0500

    modal can be closed now

--- client/app/components/GifModal.js
+++ client/app/components/GifModal.js
@@ -2,23 +2,24 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const GifModal = (props) => {
-    if (!props.selectedGif) {
-        return <div></div>;
+    if (!props.selectedGif && props.modalIsOpen==false) {
+        return <Modal contentLabel={"empty"} className="empty"></Modal>;
+    }else{
+        return (
+            <Modal
+                isOpen={ props.modalIsOpen }
+                contentLabel={"valor"}
+                onRequestClose={ () => props.onRequestClose() }>
+                <div className="gif-modal">
+                    <img src={ props.selectedGif.images.original.url } alt="" />
+                    <p><strong>Source:</strong> <a href={ props.selectedGif.source }>{ props.selectedGif.source }</a></p>
+                    <p><strong>Rating:</strong> { props.selectedGif.rating }</p>
+
+                    <button type="button" className="btn btn-danger" onClick={() => props.onRequestClose()}>close</button>
+                </div>
+            </Modal>
+        );
     }
-
-    return (
-        <Modal
-            isOpen={ props.modalIsOpen }
-            onRequestClose={ () => props.onRequestClose() }>
-            <div className="gif-modal">
-                <img src={ props.selectedGif.images.original.url } alt="" />
-                <p><strong>Source:</strong> <a href={ props.selectedGif.source }>{ props.selectedGif.source }</a></p>
-                <p><strong>Rating:</strong> { props.selectedGif.rating }</p>
-
-                <button onClick={() => props.onRequestClose()}>close</button>
-            </div>
-        </Modal>
-    );
 };
 
 export default GifModal;
