d95ed8aa02ab7f7aee22826e864c2d6e6e00a465
KanonVitage <guido.duchi@gmail.com>
2018-09-14 19:30:23 -0500

    microservices start

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+	db: 'mongodb://root:radwimps4@localhost:27017/my_db_name',
+  	db_dev: 'mongodb://root:radwimps4@localhost:27017/my_db_name',
+};
